2f6e248a48b38ff49cdee19073baf061ffb82f07
Max_McVay <mhmcvay@csuchico.edu>
2023-04-12 11:33:36 -0700

    added password unit tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,7 +20,41 @@ TEST(PasswordTest, smoke_test)
 }
 TEST(PasswordTest, single_letter_password)
 {
-    Password my_password;
+  Password my_password;
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1,actual);
+}
+
+TEST(PasswordTest, count_lead_ab)
+{
+	Password TestObject;
+	string word = "ab";
+	ASSERT_EQ(TestObject.count_leading_characters(word), 1);
+}
+TEST(PasswordTest, count_lead3)
+{
+	Password TestObject;
+	string word = "!!!hello";
+	ASSERT_EQ(TestObject.count_leading_characters(word), 3);
+}
+
+TEST(PasswordTest, count_lead_classic)
+{
+	Password TestObject;
+	string word = "Hello!";
+	ASSERT_EQ(TestObject.count_leading_characters(word), 1);
+}
+
+
+TEST(PasswordTest, count_lead_ws)
+{
+	Password TestObject;
+	string word = "  ";
+	ASSERT_EQ(TestObject.count_leading_characters(word), 2);
+}
+TEST(PasswordTest, count_lead_funky_chars)
+{
+	Password TestObject;
+	string word = "@&^mka";
+	ASSERT_EQ(TestObject.count_leading_characters(word), 1);
 }
